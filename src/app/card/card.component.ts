09c969e6e2139975f8920d857fe6711e0899ce68
Daria Korytova <dk17march@gmail.com>
2020-12-01 21:41:27 +0300

    first try 3

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -9,4 +9,8 @@ export class CardComponent {
   title = 'New Title'
   text = 'Home is behind, the world ahead'
   number = '42'
+
+  imgUrl = 'https://i.ebayimg.com/images/g/IlUAAOSwBPlc~NtR/s-l300.jpg'
+
+  getInfo(): string { return 'whoooooa' }
 }
